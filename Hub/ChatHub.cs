d75d178735e4f13ca990d9ddec634f04b5feb92d
marti <martibook@microsoft.com>
2021-12-17 16:36:14 +0800

    init SignalR Service Exmaple

---
+++ Hub/ChatHub.cs
@@ -0,0 +1,15 @@
+﻿namespace chatroom.Hub
+{
+    using Microsoft.AspNetCore.SignalR;
+    using System.Threading.Tasks;
+
+    public class ChatHub : Hub
+    {
+        public Task BroadcastMessage(string name, string message) =>
+            Clients.All.SendAsync("broadcastMessage", name, message);
+
+        public Task Echo(string name, string message) =>
+            Clients.Client(Context.ConnectionId)
+                   .SendAsync("echo", name, $"{message} (echo from server)");
+    }
+}
