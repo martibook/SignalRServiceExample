8bd26dd2619a7f55bf07f7e4e7f1e9f93253c1ec
marti <martibook@microsoft.com>
2021-12-20 15:30:56 +0800

    Device Experiments

--- Hub/ChatHub.cs
+++ Hub/ChatHub.cs
@@ -5,11 +5,18 @@
 
     public class ChatHub : Hub
     {
-        public Task BroadcastMessage(string name, string message) =>
-            Clients.All.SendAsync("broadcastMessage", name, message);
+        public Task BroadcastMessage(string name, string message)
+        {
+            System.Diagnostics.Debug.WriteLine("Send - First letter of the message [" + message[0] +"]");
+            return Clients.All.SendAsync("broadcastMessage", name, message);
+        }
+            
 
-        public Task Echo(string name, string message) =>
-            Clients.Client(Context.ConnectionId)
-                   .SendAsync("echo", name, $"{message} (echo from server)");
+        public Task Echo(string name, string message)
+        {
+            System.Diagnostics.Debug.WriteLine("Echo - First letter of the message [" + message[0] + "]");
+            return Clients.Client(Context.ConnectionId)
+                .SendAsync("echo", name, $"{message} (echo from server)");
+        } 
     }
 }
